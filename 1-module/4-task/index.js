c868e1cb09bcdfba3f42ec4e5ebae1b4ba24e0a1
Mykyta_Prus <mike.prus@ekreative.com>
2021-02-06 09:01:25 +0200

    solve 3, 4, 5 tasks

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,9 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-  // ваш код...
+  const lowerStr = str.toLowerCase();
+	if(lowerStr.includes('xxx') || lowerStr.includes('1xbet')){
+  	return true;
+  }
+  return false;
 }
