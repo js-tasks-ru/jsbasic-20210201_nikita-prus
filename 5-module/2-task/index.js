6537de383a0c217af665bbc736a3a54b3d71021b
Mykyta_Prus <mike.prus@ekreative.com>
2021-02-23 08:01:57 +0200

    solve all tasks from 5-module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,6 @@
 function toggleText() {
-  // ваш код...
+  const btn = document.querySelector('.toggle-text-button');
+  btn.addEventListener('click', (event) => {
+    text.hidden = !text.hidden;
+  });
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,48 @@
 function initCarousel() {
-  // ваш код...
+  const nextBtn = document.querySelector('.carousel__arrow_right');
+  const prevBtn = document.querySelector('.carousel__arrow_left');
+  const slider = document.querySelector('.carousel__inner');
+  const slides = slider.children;
+  const slideWidth = slider.children[0].offsetWidth;
+  let transformX = 0;
+
+
+  const hideButtons = (slidePosition, lastSlidePosition) => {
+    if(slidePosition === 0){
+      prevBtn.style.display = 'none';
+    } else if(Math.abs(slidePosition) === lastSlidePosition) {
+      nextBtn.style.display = 'none';
+    } else {
+      nextBtn.style.display = '';
+      prevBtn.style.display = '';
+    }
+  }
+
+  const getLastSlideOffset = () => {
+    let totalSlidesWidth = 0;
+
+    for(let slide of slides){
+      totalSlidesWidth += slide.offsetWidth;
+    }
+
+    return totalSlidesWidth -= slideWidth;
+  }
+
+  const lastSlideOffset = getLastSlideOffset();
+
+
+  nextBtn.addEventListener('click', () => {
+    transformX -= slideWidth;
+    slider.style.transform = `translateX(${transformX}px)`;
+    hideButtons(transformX, lastSlideOffset);
+  });
+
+  prevBtn.addEventListener('click', () => {
+    transformX += slideWidth;
+    slider.style.transform = `translateX(${transformX}px)`;
+    hideButtons(transformX, lastSlideOffset);
+  });
+
+
+  hideButtons(transformX, lastSlideOffset)
 }
