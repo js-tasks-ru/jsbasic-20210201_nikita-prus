4d23e335a878cafd68ca64a7af679ed528349935
Mykyta_Prus <mike.prus@ekreative.com>
2021-02-27 10:59:20 +0200

    solve 1 task of 6 module

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,46 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = document.createElement('table');
+    this.generateTBody();
+    this.generateTHead();
+    this.deleteTRow();
+  }
+
+  generateTHead(){
+    const thead = this.elem.createTHead();
+    const row = thead.insertRow();
+
+    for(let key in this.rows[0]){
+      let th = document.createElement('th');
+      let text = document.createTextNode(key);
+      th.appendChild(text);
+      row.appendChild(th);
+    }
+  }
+
+
+  generateTBody(){
+    for(let element of this.rows){
+      let row = this.elem.insertRow();
+      for(let key in element){
+        let cell = row.insertCell();
+        let text = document.createTextNode(element[key]);
+        cell.appendChild(text);
+      }
+      row.insertAdjacentHTML('beforeend', '<td><button>X</button></td>');
+    }
+  }
+
+  deleteTRow(){
+    this.elem.onclick = (event) => {
+      let target = event.target;
+
+      if(target.tagName === 'BUTTON'){
+        const row = target.parentElement.parentElement;
+        this.elem.deleteRow(row.rowIndex);
+      }
+    }
   }
 }
