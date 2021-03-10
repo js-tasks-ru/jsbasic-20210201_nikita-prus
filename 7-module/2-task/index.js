29166f101ef6b0f60d590359bef3d3276ed8bec4
Mykyta_Prus <mike.prus@ekreative.com>
2021-03-10 09:52:47 +0200

    work in progress on task 2 of module 7

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,16 +1,14 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
-  modal;
-
   constructor() {
-    this.createModal();
+    this.modal = this.createModal();
   }
 
   setTitle(title){
     const modalTitle = this.modal.querySelector('.modal__title');
     if(modalTitle){
-      modalTitle.innerText = title;
+      modalTitle.textContent = title;
     }
   }
 
@@ -38,24 +36,24 @@ export default class Modal {
       </div>
     </div>`
 
-   this.modal = createElement(html);
+   return createElement(html);
   }
 
   open(){
     document.body.classList.add('is-modal-open');
     document.body.appendChild(this.modal);
-    this.close(this.modal);
   }
 
-  close(modal){
-    const closeBtn = modal.querySelector('.modal__close');
+  close(){
     const removeModal = () => {
-      modal.remove();
       document.body.removeAttribute('class');
+      this.modal.remove();
     }
 
-    closeBtn.addEventListener('click', () => {
-      removeModal();
+    document.addEventListener('click', (event) => {
+      if(event.target.alt === 'close-icon'){
+        removeModal();
+      }
     });
 
     document.addEventListener('keydown', (event) => {
